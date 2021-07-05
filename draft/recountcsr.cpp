b6c7b5b819511892e50ff46c7bede8ada42bb934
dnbh <dnb@cs.jhu.edu>
2021-07-05 18:40:29 -0400

    Draft

--- draft/recountcsr.cpp
+++ draft/recountcsr.cpp
@@ -29,6 +29,7 @@ int str2ms(const char *s) {
 }
 
 auto parse_file(std::FILE *ifp) {
+    std::fprintf(stderr, "Starting\n");
     std::vector<uint32_t> contigids;
     std::vector<uint32_t> starts, stops;
     std::vector<uint8_t> startms, stopms;
@@ -40,9 +41,13 @@ auto parse_file(std::FILE *ifp) {
     size_t linesz = 0;
     std::string cname;
     size_t ln = 0;
-    for(;std::feof(ifp);++ln) {
-        if(ln % 65536 == 0) std::fprintf(stderr, "Processed %zu lines\n", ln);
-        if(::getline(&lptr, &linesz, ifp) < 0) throw 1;
+    ssize_t rc;
+    for(;
+        //std::feof(ifp)
+        ;++ln) {
+        if((rc = ::getline(&lptr, &linesz, ifp)) < 0) throw 1;
+        if(ln % 1 == 0) std::fprintf(stderr, "Processed %zu lines, last rc is %zd\n", ln, rc);
+        std::fputs(lptr, stdout);
         const uint64_t myid = idcounter++;
         
         char *p = std::strchr(lptr, '\t');
@@ -60,18 +65,25 @@ auto parse_file(std::FILE *ifp) {
         p = std::strchr(p2 + 1, '\t');
         ssize_t srpos = std::strtoll(p, &p, 10);
         ssize_t sppos = std::strtoll(p, &p, 10);
+        std::fprintf(stderr, "poses: %zd/%zd\n", srpos, sppos);
         p = std::strchr(p + 1, '\t'); // Skip the length field
-        assert(p[1] == '+' || p[1] == '-');
+        assert(p[1] == '+' || p[1] == '-' || !std::fprintf(stderr, "p: %s, %d\n", p, p[1]));
         strand.push_back(p[1] == '+');
         p += 3;
         startms.push_back(str2ms(p));
         p = std::strchr(p, '\t') + 1;
+        std::fprintf(stderr, "p for ms: %s\n", p);
         stopms.push_back(str2ms(p));
         p = std::strchr(std::strchr(p, '\t') + 1, '\t') + 1; // Skip two fields
+        std::fprintf(stderr, "P: '%s'\n", p);
+        std::exit(1);
     }
     return std::make_tuple(contigids, starts, stops, startms, stopms, strand, contignames);
 }
 
-int main() {
-    auto [cids, starts, stops, startms, stopms, strand, contignames] = parse_file(stdin);
+int main(int argc, char **argv) {
+    std::FILE *ifp = std::fopen(argc == 1 ? "/dev/stdin": argv[1], "r");
+    auto [cids, starts, stops, startms, stopms, strand, contignames] = parse_file(ifp);
+    std::fclose(ifp);
+    return 0;
 }
