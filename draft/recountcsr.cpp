3f27127253e88d526df7bde21aa8997647df5b48
Daniel N. Baker <dnb@cs.jhu.edu>
2021-07-05 19:07:14 -0400

    recountcsr completed

--- draft/recountcsr.cpp
+++ draft/recountcsr.cpp
@@ -31,10 +31,12 @@ int str2ms(const char *s) {
 auto parse_file(std::FILE *ifp) {
     std::fprintf(stderr, "Starting\n");
     std::vector<uint32_t> contigids;
+    std::vector<uint64_t> ids;
     std::vector<uint32_t> starts, stops;
     std::vector<uint8_t> startms, stopms;
     std::atomic<uint64_t> idcounter;
-    std::vector<bool> strand;
+    std::vector<uint16_t> counts;
+    std::vector<uint64_t> indptr{0};
     ska::flat_hash_map<std::string, uint32_t> contignames;
     idcounter.store(0);
     char *lptr = nullptr;
@@ -42,12 +44,8 @@ auto parse_file(std::FILE *ifp) {
     std::string cname;
     size_t ln = 0;
     ssize_t rc;
-    for(;
-        //std::feof(ifp)
-        ;++ln) {
-        if((rc = ::getline(&lptr, &linesz, ifp)) < 0) throw 1;
-        if(ln % 1 == 0) std::fprintf(stderr, "Processed %zu lines, last rc is %zd\n", ln, rc);
-        std::fputs(lptr, stdout);
+	for(ssize_t rc; (rc = ::getline(&lptr, &linesz, ifp)) >= 0;++ln) {
+        if(ln % 1 == 65536) std::fprintf(stderr, "Processed %zu lines, last rc is %zd\n", ln, rc);
         const uint64_t myid = idcounter++;
         
         char *p = std::strchr(lptr, '\t');
@@ -62,28 +60,67 @@ auto parse_file(std::FILE *ifp) {
         if(cnit != contignames.end()) mycid = cnit->second;
         else contignames.emplace(cname, mycid);
         contigids.push_back(mycid);
-        p = std::strchr(p2 + 1, '\t');
+        p = p2 + 1;
         ssize_t srpos = std::strtoll(p, &p, 10);
         ssize_t sppos = std::strtoll(p, &p, 10);
-        std::fprintf(stderr, "poses: %zd/%zd\n", srpos, sppos);
         p = std::strchr(p + 1, '\t'); // Skip the length field
-        assert(p[1] == '+' || p[1] == '-' || !std::fprintf(stderr, "p: %s, %d\n", p, p[1]));
-        strand.push_back(p[1] == '+');
-        p += 3;
+        assert(p[1] == '+' || p[1] == '-' || p[1] == '?' || !std::fprintf(stderr, "p: %s, %d\n", p, p[1]));
+        //p += 3;
+        p = std::strchr(p + 3, '\t') + 1;
         startms.push_back(str2ms(p));
         p = std::strchr(p, '\t') + 1;
-        std::fprintf(stderr, "p for ms: %s\n", p);
         stopms.push_back(str2ms(p));
-        p = std::strchr(std::strchr(p, '\t') + 1, '\t') + 1; // Skip two fields
-        std::fprintf(stderr, "P: '%s'\n", p);
-        std::exit(1);
+        p = std::strchr(std::strchr(std::strchr(p, '\t') + 1, '\t') + 1, '\t') + 1; // Skip two fields
+        uint64_t id;
+        int32_t ct;
+        size_t nids = 0;
+        for(char *p3 = p;*p3 && *p3 != '\t';) {
+            id = std::strtoull(p3 + 1, &p3, 10);
+            ct = std::strtoll(p3 + 1, &p3, 10);
+            counts.push_back(ct);
+            ids.push_back(id);
+            ++nids;
+        }
+        indptr.push_back(nids);
     }
-    return std::make_tuple(contigids, starts, stops, startms, stopms, strand, contignames);
+    return std::make_tuple(contigids, contignames, counts, ids, indptr);
 }
 
 int main(int argc, char **argv) {
-    std::FILE *ifp = std::fopen(argc == 1 ? "/dev/stdin": argv[1], "r");
-    auto [cids, starts, stops, startms, stopms, strand, contignames] = parse_file(ifp);
-    std::fclose(ifp);
+    std::FILE *fp = std::fopen(argc == 1 ? "/dev/stdin": argv[1], "r");
+    auto [cids, cnames, counts, ids, indptr] = parse_file(fp);
+    std::fclose(fp);
+
+    ska::flat_hash_map<uint64_t, uint16_t> mapper;
+    for(const auto id: ids) {
+        if(mapper.find(id) == mapper.end()) {
+            auto oldsz = mapper.size();
+            mapper.emplace(id, oldsz);
+        }
+    }
+    assert(mapper.size() < 65536);
+    std::transform(ids.begin(), ids.end(), ids.begin(), [&mapper](auto x) {return mapper[x];});
+    fp = std::fopen("parsed.cts.u16", "wb");
+    std::fwrite(counts.data(), 2, counts.size(), fp);
+    std::fclose(fp);
+    fp = std::fopen("parsed.ids.u16", "wb");
+    for(const auto id: ids) {
+        uint16_t sid(id);
+        std::fwrite(&sid, 2, 1, fp);
+    }
+    std::fclose(fp);
+    fp = std::fopen("parsed.indptr.u16", "w");
+    std::fwrite(indptr.data(), sizeof(indptr[0]), indptr.size(), fp);
+    std::fclose(fp);
+
+    for(const auto &pair: mapper)
+        std::fprintf(fp, "%zu:%zu\n", size_t(pair.first), size_t(pair.second));
+    std::fclose(fp);
+
+    fp = std::fopen("parsed.remap", "w");
+    for(const auto &pair: mapper)
+        std::fprintf(fp, "%zu:%zu\n", size_t(pair.first), size_t(pair.second));
+    std::fclose(fp);
+
     return 0;
 }
