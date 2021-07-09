98d4195244b57aa814556a654edf4a6a6a48381f
dnbh <dnb@cs.jhu.edu>
2021-07-09 14:49:39 -0400

    Usage

--- src/d2.cpp
+++ src/d2.cpp
@@ -96,7 +96,7 @@ int main_usage() {
     std::fprintf(stderr, "Usage can be seen in those commands.\n");
     std::fprintf(stderr, "cmp: compres previously sketched/decomposed k-mer sets and emits results.\n");
     std::fprintf(stderr, "sketch: converts FastX into k-mer sets/sketches, and sketches BigWig and BED files; also contains functionality from cmp, for one-step sketch and comparisons\n");
-    std::fprintf(stderr, "wsketch: Takes a pair of input binary files [(float or double), (u32 or u32)] and performs weighted minhash sketching.\n");
+    std::fprintf(stderr, "wsketch: Takes a tuple of [1-3] input binary files [(u32 or u64), (float or double), (u32 or u64)] and performs weighted minhash sketching.\n");
     return 1;
 }
 using namespace dashing2;
