492d476e50cdf1b63abe7d3469699cbe02294e2d
Daniel Baker <dnb@cs.jhu.edu>
2021-09-03 13:25:06 -0400

    J thresholding bug fix, mmap improvements. (#32)
    * Finish contain method correction.
    
    * Update bns
    
    * b5 fix
    
    * Usage for contain
    
    * Update bonsai
    
    * Cascade Lake propagation.
    
    * Use std::fopen instead of ::popen('cat '...'), then close after.
    
    * Work on compressedreps fix
    
    * Check if outfile has been specified to avoid incorrect files being written.
    
    * Attempts to prevent memory corruption error
    
    * Fix for memory issue.
    
    * Use aligned allocator for RAM-based vectors
    
    * Handle edge case with existing mmap'd file. If MAP_HUGETLB causes mmap to fail, fall back to non-huge-paged version.
    
    * Correct fallback
    
    * Correction to JI-thresholded results.
    
    * Remove debug log about failed huge pages allocation

--- src/refine.cpp
+++ src/refine.cpp
@@ -1,6 +1,8 @@
 #include "refine.h"
 namespace dashing2 {
 
+static constexpr LSHDistType MDIST = std::numeric_limits<LSHDistType>::max();
+
 void refine_results(std::vector<pqueue> &lists, const Dashing2DistOptions &opts, const SketchingResult &result) {
     //LSHDistType compare(Dashing2DistOptions &opts, const SketchingResult &result, size_t i, size_t j);
     const LSHDistType mult = distance(opts.measure_) ? 1.: -1.;
@@ -36,9 +38,9 @@ void refine_results(std::vector<pqueue> &lists, const Dashing2DistOptions &opts,
             if(size_t(opts.num_neighbors_) < l.size() - 1)
                 l.resize(opts.num_neighbors_);
         } else if(opts.min_similarity_ > 0.) {
+            static constexpr size_t EARLY_FAILURE_EXIT_THRESHOLD = 25u;
             // -- as above, for the NN_GRAPH_THRESHOLD format
-            //std::fprintf(stderr, "Filtering for minimum similarity\n");
-            // This stopping after 5 consecutive beyond-threshold points is purely heuristic
+            // This stopping after `EARLY_FAILURE_EXIT_THRESHOLD` consecutive beyond-threshold points is purely heuristic
             // and may change.
             size_t failures = 0;
             for(size_t j = 0; j < lsz; ++j) {
@@ -46,8 +48,8 @@ void refine_results(std::vector<pqueue> &lists, const Dashing2DistOptions &opts,
                 auto v = compare(opts, result, lhid, id);
                 const bool pass = distance(opts.measure_) ? v < opts.min_similarity_: v >= opts.min_similarity_;
                 if(!pass) {
-                    dist = std::numeric_limits<LSHDistType>::max();
-                    if(++failures == 5) {
+                    dist = MDIST;
+                    if(++failures == EARLY_FAILURE_EXIT_THRESHOLD) {
                         l.resize(j);
                         break;
                     }
@@ -56,11 +58,15 @@ void refine_results(std::vector<pqueue> &lists, const Dashing2DistOptions &opts,
                     failures = 0;
                 }
             }
-            std::sort(l.begin(), l.end());
-            l.erase(std::find_if(l.begin(), l.end(), [dist=distance(opts.measure_),ms=opts.min_similarity_](PairT x) {
-                return dist ? x.first < ms: -x.first >= ms;
+            l.erase(std::remove_if(l.begin(), l.end(), [dist=distance(opts.measure_),ms=opts.min_similarity_](const PairT x) -> bool {
+                return x.first == MDIST || (dist ? x.first > ms: -x.first < ms);
             }), l.end());
-            std::fprintf(stderr, "Smallest similarity: %g. threshold: %g\n", mult * l.back().first, opts.min_similarity_);
+            std::sort(l.begin(), l.end());
+#ifndef NDEBUG
+            if(!l.empty()) {
+                std::fprintf(stderr, "Smallest comparison: %g. threshold: %g\n", l.back().first, opts.min_similarity_);
+            }
+#endif
         } else {
             std::transform(beg, e, beg, [&](PairT x) -> PairT {return {mult * compare(opts, result, lhid, x.second), x.second};});
             std::sort(beg, e);
