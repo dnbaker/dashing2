fd6e4a834da86f631c3b00da55f6e89a43b1437c
Lino Hofstetter <lino@Hugo-2.fritz.box>
2024-06-08 09:23:50 +0200

    removed definition of default parameter in headerfile for make_dest function

--- src/fastxmerge.h
+++ src/fastxmerge.h
@@ -10,7 +10,7 @@ class SketchingResult;
 
 SketchingResult merge(SketchingResult *start, size_t n, const std::vector<std::string> &names = std::vector<std::string>());
 
-std::string makedest(Dashing2Options &opts, const std::string &path, bool iskmer=false);
+std::string makedest(Dashing2Options &opts, const std::string &path, bool iskmer);
 
 }
 
